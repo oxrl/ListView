a68f5cd4d0d94b2265ac46cf9815dc04095ef3f7
Xavier Romero Lopez <o.romero.lopez@gmail.com>
2019-04-28 05:01:31 -0500

    Se testeo el onClick en un boton

--- app/src/main/java/com/example/viewcomponents/MainActivity.java
+++ app/src/main/java/com/example/viewcomponents/MainActivity.java
@@ -1,13 +1,30 @@
-   package com.example.viewcomponents;
+package com.example.viewcomponents;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+   public class MainActivity extends AppCompatActivity implements OnClickListener {
+
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.buttonLogin);
+        btnLogin.setOnClickListener(this);
+
+
     }
-}
+
+       @Override
+       public void onClick(View v) {
+           Log.i("APP","Click en Login" );
+
+       }
+   }
