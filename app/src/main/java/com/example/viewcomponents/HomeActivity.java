08750e44e105cd2e9ef1ac9f67d611b6644e2bbf
Xavier Romero Lopez <o.romero.lopez@gmail.com>
2019-04-30 23:56:16 -0500

    Se Creo un Activity adiciona llamado Home y se lo configuro con el un ViewText

---
+++ app/src/main/java/com/example/viewcomponents/HomeActivity.java
@@ -0,0 +1,18 @@
+package com.example.viewcomponents;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        TextView textEmail;
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        textEmail = findViewById(R.id.textViewEmail);
+        textEmail.setText("Bienvenido");
+    }
+}
--- app/src/main/java/com/example/viewcomponents/MainActivity.java
+++ app/src/main/java/com/example/viewcomponents/MainActivity.java
@@ -1,22 +1,28 @@
 package com.example.viewcomponents;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 
-   public class MainActivity extends AppCompatActivity implements OnClickListener {
+public class MainActivity extends AppCompatActivity implements OnClickListener {
 
     Button btnLogin;
-
+    EditText editTextEmail , editTextPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         btnLogin = findViewById(R.id.buttonLogin);
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+
+
         btnLogin.setOnClickListener(this);
 
 
@@ -25,6 +31,15 @@ import android.widget.Button;
        @Override
        public void onClick(View v) {
            Log.i("APP","Click en Login" );
+           String email = editTextEmail.getText().toString();
+           String pass  = editTextPassword.getText().toString();
+
+           if(!email.isEmpty() && !pass.isEmpty()){
+               Intent intentLogin = new Intent(this,HomeActivity.class);
+               intentLogin.putExtra("valorEmail",email);
+               startActivity(intentLogin);
+           }
+
 
        }
    }
